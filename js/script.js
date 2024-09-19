06244392971b3d74448ce2a93700b99ef24221ac
UncleeJoo16 <jonathansatria16@gmail.com>
2024-09-19 10:02:20 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//setting toggle
+
+const navbarnav = document.querySelector(".navbar-nav");
+
+//klik
+document.querySelector("#menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+//slideshow gambar
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//auto 5 detik
+showSlidess();
+function showSlidess() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlidess, 5000);
+}
